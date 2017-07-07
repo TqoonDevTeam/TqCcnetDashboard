ccd0338454d540d8c65fabff42ca221f0676e0f2
wisdomcd <wisdomcdh@gmail.com>
2017-07-07 14:52:36 +0900

    dd

--- tqdashboardUpdate/web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TqCcnetDashboard.MvcApplication" Language="C#" %>
--- tqdashboardUpdate/web/Scripts/angularAMD-ngload.js
+++
@@ -1,9 +0,0 @@
-﻿define({
-    load: function (name, req, onload) {
-        'use strict';
-        req(['angularAMD', name], function (angularAMD, value) {
-            angularAMD.processQueue();
-            onload(value);
-        });
-    }
-});
