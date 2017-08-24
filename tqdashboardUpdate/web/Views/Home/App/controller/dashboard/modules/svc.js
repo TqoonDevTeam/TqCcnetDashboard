83ecc4ad45b9b2db95758e60e1cbe8a7c6a4164d
wisdomcd <wisdomcdh@gmail.com>
2017-08-24 14:39:52 +0900

    3

--- tqdashboardUpdate/web/Views/Home/App/controller/dashboard/modules/svc.js
+++
@@ -1,8 +0,0 @@
-﻿define(['app'], function (app) {
-    app.service('dashboard.svc', ['$http', 'svcUtil', function ($http, svcUtil) {
-        var svc = {
-            Dashboard: svcUtil.create('Dashboard', true).$action('GetProjectStatus').$action('ForceBuild').$action('AbortBuild')
-        };
-        return svc;
-    }])
-});
--- tqdashboardUpdate/web/Views/Home/App/controller/settings.plugins.js
+++
@@ -1,4 +0,0 @@
-﻿define(['app'], function (app) {
-    app.controller('settings.pluginsCtrl', ['$scope', '$http', function ($scope, $http) {
-    }]);
-});
