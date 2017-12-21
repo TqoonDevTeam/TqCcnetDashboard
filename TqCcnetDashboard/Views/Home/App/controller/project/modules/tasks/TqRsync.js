2e6cd915a34b07007ebe1ebd51cd7104cbf67ff7
wisdomcd <wisdomcdh@gmail.com>
2017-12-21 15:01:01 +0900

    timeout force

--- TqCcnetDashboard/Views/Home/App/controller/project/modules/tasks/TqRsync.js
+++ TqCcnetDashboard/Views/Home/App/controller/project/modules/tasks/TqRsync.js
@@ -1,7 +1,7 @@
 ﻿define(['app'], function (app) {
     'use strict';
     app.controller('project.step4.tasks.TqRsync.customctrl', ['$scope', function ($scope) {
-        $scope.custom.attrs_force_show = ['workingDirectory'];
+        $scope.custom.attrs_force_show = ['workingDirectory', 'timeout'];
         $scope.custom.attrs_force_required = [];
         $scope.custom.template = {};
         $scope.custom.defaultValue = {
