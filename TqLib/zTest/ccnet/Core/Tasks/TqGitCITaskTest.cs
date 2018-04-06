dda9a92b66c0a5a20230cdbb465055f8eb312937
wisdomcd <wisdomcdh@gmail.com>
2018-04-06 18:44:12 +0900

    개발

---
+++ TqLib/zTest/ccnet/Core/Tasks/TqGitCITaskTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Remote;
+using TqLib.ccnet.Core.Tasks;
+
+namespace TqLib.zTest.ccnet.Core.Tasks
+{
+    [TestFixture(Category = "ccnet.core.Tasks")]
+    public class TqGitCITaskTest
+    {
+        [Test]
+        [Ignore("로컬전용")]
+        public void TqGitCITask()
+        {
+            var result = new IntegrationResult("test", @"D:\Test", @"D:\Test", new IntegrationRequest(BuildCondition.ForceBuild, "", ""), new IntegrationSummary(IntegrationStatus.Success, "", "", DateTime.Now));
+
+            TqGitCITask task = new TqGitCITask();
+            task.GitRepository = "https://github.com/wisdomcdh/Test.git";
+
+            task.Branch = "dev";
+            task.StartBranch = "master";
+
+            task.Run(result);
+        }
+    }
+}
